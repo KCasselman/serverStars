748460c0491e3cbd1844d119ec71f7c12d7038b1
Kelly Williams <Kewilliams591@gmail.com>
2018-11-13 12:41:17 -0500

    yep

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 app.use('/goal', Goal);
-app.use(require('./middleware/headers'));
+app.use(require('./Middleware/headers'));
 app.use('/user', user);
 
 require('./associations.js')
